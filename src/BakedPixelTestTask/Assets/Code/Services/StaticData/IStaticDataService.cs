10f46cc236d3c61d185bc9ad77a751a51c36f960
Olsney <104034829+Olsney@users.noreply.github.com>
2025-07-18 01:58:52 +0300

    Initialized static data service

--- src/BakedPixelTestTask/Assets/Code/Services/StaticData/IStaticDataService.cs
+++ src/BakedPixelTestTask/Assets/Code/Services/StaticData/IStaticDataService.cs
@@ -1,6 +1,12 @@
+using System.Collections.Generic;
+using Code.StaticData.Item;
+
 namespace Code.Services.StaticData
 {
     public interface IStaticDataService
     {
+        void LoadItems();
+        ItemConfig ForItem(string id);
+        IReadOnlyList<ItemConfig> AllItems { get; }
     }
 }
--- src/BakedPixelTestTask/Assets/Code/Services/StaticData/StaticDataService.cs
+++ src/BakedPixelTestTask/Assets/Code/Services/StaticData/StaticDataService.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
+using Code.StaticData.Item;
 using UnityEngine;
 
 namespace Code.Services.StaticData
 {
     public class StaticDataService : IStaticDataService
     {
+        private const string ItemsPath = "Configs/Items";
         
+        private Dictionary<string, ItemConfig> _items;
+
+        public IReadOnlyList<ItemConfig> AllItems => _items.Values.ToList();
+
+        public void LoadItems()
+        {
+            _items = Resources
+                .LoadAll<ItemConfig>(ItemsPath)
+                .ToDictionary(x => x.Id, x => x);
+        }
+
+        public ItemConfig ForItem(string id)
+        {
+            return _items.TryGetValue(id, out var config)
+                ? config
+                : null;
+        }
     }
 }
